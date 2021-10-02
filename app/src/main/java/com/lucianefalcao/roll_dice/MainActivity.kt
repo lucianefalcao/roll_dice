427d8e3f405b49b81725d886f72a49104541e0cd
Luciane Falcão <lucianefalcaoff@gmail.com>
2021-10-02 19:20:34 -0300

    add dice images and change when the user clicks the button

--- app/src/main/java/com/lucianefalcao/roll_dice/MainActivity.kt
+++ app/src/main/java/com/lucianefalcao/roll_dice/MainActivity.kt
@@ -4,11 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
-import android.widget.TextView
-import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
-    val diceImages = listOf<Int>(R.drawable.dice_1, R.drawable.dice_2, R.drawable.dice_3, R.drawable.dice_4, R.drawable.dice_5, R.drawable.dice_6);
+    private val diceImages = listOf(R.drawable.dice_1, R.drawable.dice_2, R.drawable.dice_3,
+        R.drawable.dice_4, R.drawable.dice_5, R.drawable.dice_6)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
