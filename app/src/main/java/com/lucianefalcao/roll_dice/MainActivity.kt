5a510d0055ed594e14df2944461768aa54e90739
Luciane Falcão <lucianefalcaoff@gmail.com>
2021-10-02 17:24:30 -0300

    fix #2 add function to change the dice number dynamically

--- app/src/main/java/com/lucianefalcao/roll_dice/MainActivity.kt
+++ app/src/main/java/com/lucianefalcao/roll_dice/MainActivity.kt
@@ -2,10 +2,18 @@ package com.lucianefalcao.roll_dice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun rollDice(view: View) {
+        val diceNumber = findViewById<TextView>(R.id.dice)
+        diceNumber.text = (1..6).random().toString()
     }
 }
